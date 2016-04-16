5e4cfca119740304220c1698ffc1ad255cd9b7cd
Preetam Jinka <pj@preet.am>
2016-04-16 17:30:27 -0400

    request recaptcha

--- web/main.go
+++ web/main.go
@@ -13,6 +13,9 @@ import (
 )
 
 var (
+	// RecaptchaSecret is the API secret used to verify reCHAPTCHA responses
+	RecaptchaSecret = ""
+
 	templ *template.Template
 )
 
@@ -20,6 +23,7 @@ func main() {
 	addr := flag.String("addr", ":4003", "Listen address")
 	staticDir := flag.String("static-dir", "./static", "Path to static content")
 	templatesDir := flag.String("templates-dir", "./templates", "Path to templates")
+	flag.StringVar(&RecaptchaSecret, "recaptcha-secret", "", "reCHAPTCHA API secret")
 	flag.Parse()
 
 	var err error
@@ -36,7 +40,8 @@ func main() {
 		templ.ExecuteTemplate(w, "index", nil)
 	})
 
-	service.Route("GET", "/l/:link", "serves link requests", serveGetRequest)
+	service.Route("GET", "/r/:link", "serves contact requests", serveGetRequest)
+	service.Route("POST", "/r/:link", "serves contact submissions", servePostRequest)
 
 	service.SetNotFound(http.FileServer(http.Dir(*staticDir)))
 	log.Println("static directory set to", *staticDir)
--- web/requests.go
+++ web/requests.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/json"
+
 	"github.com/VividCortex/siesta"
 
 	"net/http"
+	"net/url"
 	"strings"
 )
 
@@ -31,4 +34,84 @@ func serveGetRequest(w http.ResponseWriter, r *http.Request) {
 	templ.ExecuteTemplate(w, "request", map[string]string{
 		"Name": "John Doe",
 	})
+}
+
+func servePostRequest(w http.ResponseWriter, r *http.Request) {
+	params := &siesta.Params{}
+	recaptchaResponse := params.String("g-recaptcha-response", "", "reCAPTCHA response")
+	nameStr := params.String("name", "", "name")
+	emailStr := params.String("email", "", "email")
+	linkStr := params.String("link", "", "link code")
+	err := params.Parse(r.Form)
+
+	invalidLink := func() {
+		w.WriteHeader(http.StatusBadRequest)
+		templ.ExecuteTemplate(w, "invalid", map[string]string{
+			"Error": "Not a valid OneContactLink",
+		})
+		return
+	}
+
+	if err != nil || !strings.Contains(*linkStr, "-") {
+		invalidLink()
+	}
+	parts := strings.Split(*linkStr, "-")
+	if len(parts) != 2 {
+		invalidLink()
+	}
+
+	if *recaptchaResponse == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		templ.ExecuteTemplate(w, "invalid", map[string]string{
+			"Error": "Bad CAPTCHA",
+		})
+		return
+	}
+
+	if *nameStr == "" || *emailStr == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		templ.ExecuteTemplate(w, "request", map[string]string{
+			"Error": "You must provide a name and email address",
+		})
+		return
+	}
+
+	// verify CAPTCHA
+	resp, err := http.PostForm("https://www.google.com/recaptcha/api/siteverify", url.Values{
+		"secret":   []string{RecaptchaSecret},
+		"response": []string{*recaptchaResponse},
+	})
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		templ.ExecuteTemplate(w, "request", map[string]string{
+			"Name":    "John Doe",
+			"Warning": "Something went wrong. Please try again.",
+		})
+		return
+	}
+	recaptchaAPIResponse := struct {
+		Success bool `json:"success"`
+	}{}
+
+	defer resp.Body.Close()
+	err = json.NewDecoder(resp.Body).Decode(&recaptchaAPIResponse)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		templ.ExecuteTemplate(w, "request", map[string]string{
+			"Name":    "John Doe",
+			"Warning": "Something went wrong. Please try again.",
+		})
+		return
+	}
+	if recaptchaAPIResponse.Success {
+		templ.ExecuteTemplate(w, "success", map[string]string{
+			"Success": "Request sent!",
+		})
+	} else {
+		w.WriteHeader(http.StatusBadRequest)
+		templ.ExecuteTemplate(w, "invalid", map[string]string{
+			"Error": "Bad CAPTCHA",
+		})
+		return
+	}
 }
