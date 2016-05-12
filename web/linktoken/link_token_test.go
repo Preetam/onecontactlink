bc8042f02fe192cd86220dbc75222ccacbc77a33
Preetam Jinka <pj@preet.am>
2016-05-11 23:23:22 -0400

    fix test

--- web/linktoken/link_token_test.go
+++ web/linktoken/link_token_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestLinkToken(t *testing.T) {
 	c := NewTokenCodec(1, "example key 1234")
 
-	data := map[string]interface{}{"request": 1}
+	data := map[string]interface{}{"request": float64(1)}
 	expire := 100
 	token, err := NewLinkToken(data, expire)
 	if err != nil {
@@ -15,7 +15,6 @@ func TestLinkToken(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	t.Log(res)
 
 	token, err = c.DecodeToken(res)
 	if err != nil {
@@ -26,7 +25,7 @@ func TestLinkToken(t *testing.T) {
 		t.Error("expected token.Expires to be %d, got %d", expire, token.Expires)
 	}
 
-	if _, ok := token.Data["request"].(int); !ok {
-		t.Errorf("expected token.Data[\"request\"] to be %d, got %v", 1, token.Data["request"])
+	if _, ok := token.Data["request"].(float64); !ok {
+		t.Errorf("expected token.Data[\"request\"] to be %f, got %v", 1, token.Data["request"])
 	}
 }
