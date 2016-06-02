135ec4c02bc8d2bbdfc44bb42cd79609de198b4d
Preetam Jinka <pj@preet.am>
2016-06-02 01:09:02 -0400

    set cookie domain

--- web/requests.go
+++ web/requests.go
@@ -375,6 +375,7 @@ func serveAuth(c siesta.Context, w http.ResponseWriter, r *http.Request) {
 	http.SetCookie(w, &http.Cookie{
 		Name:     "ocl",
 		Value:    token,
+		Domain:   ".onecontact.link",
 		Path:     "/",
 		HttpOnly: true,
 		Secure:   !DevMode,
