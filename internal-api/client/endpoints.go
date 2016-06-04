00eff042d57f4d9f2a59fea513121caffb699cd4
Preetam Jinka <pj@preet.am>
2016-06-04 17:50:28 -0400

    escape email in client

--- internal-api/client/endpoints.go
+++ internal-api/client/endpoints.go
@@ -3,6 +3,7 @@ package client
 import (
 	"errors"
 	"net/http"
+	"net/url"
 
 	"github.com/Preetam/onecontactlink/middleware"
 	"github.com/Preetam/onecontactlink/schema"
@@ -126,5 +127,5 @@ func (c *Client) ManageRequest(id int, action string) error {
 }
 
 func (c *Client) SendAuth(email string) error {
-	return c.doRequest("POST", "/auth/send?email="+email, nil, nil)
+	return c.doRequest("POST", "/auth/send?email="+url.QueryEscape(email), nil, nil)
 }
